7943d80a951a51240a4c627190183bff84b0693b
Darii Yar <dariiy.yar@gmail.com>
2020-07-07 21:10:27 +0300

    Plays#images, game.html

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,3 +1,14 @@
 class PlaysController < ApplicationController
-  def game; end
+  def game
+    @plays = Play.all
+  end
+
+  def images
+    service = CreatePlay.perform(params[:play])
+    if service.success?
+      render json: PlayData.perform(service.play).data, status: :created
+    else
+      render errors: service.errors, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'plays#game'
+
+  post 'images', to: "plays#images"
 end
